927120a2509980adba5c07d9aed55942c2e8fc1e
Bhakti Pasaribu <balapa95@gmail.com>
2022-11-15 05:54:32 +0700

    added resume hanger

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 const rive = require('@rive-app/canvas');
 import gsap from 'gsap'
 import { GUI } from 'dat.gui';
-import file from './../img/6.riv'
+import file from './../img/5.riv'
 import mouse from './components/mouse'
 import fixedScroll from './components/scroll'
 import * as interactions from './components/interactions'
@@ -17,8 +17,8 @@ class App {
 		this.rive
 		this.riveInputs = {}
 		this.setRive()
-
 		this.setRiveResume()
+
 
 		// interactions.fit()
 		// interactions.curve()
@@ -40,7 +40,12 @@ class App {
 				fit: rive.Fit.FitHeight
 			}),
 			autoplay: true,
-			onLoad: e => { this.playRive() },
+			onLoad: e => {
+				this.playRive()
+				// setTimeout(() => {
+				// 	this.riveResume.play()
+				// }, 300)
+			},
 			onStateChange: e => { this.stateRive(e) }
 		})
 	}
@@ -57,10 +62,11 @@ class App {
 			y: inputs.find((i) => i.name === 'translateY'),
 			sectionAbout: inputs.find((i) => i.name === 'sectionAbout'),
 			sectionIntro: inputs.find((i) => i.name === 'sectionIntro'),
-			shirt: inputs.find((i) => i.name === 'shirt'),
+			clothes: inputs.find((i) => i.name === 'clothes'),
 		}
 
 		// console.log(this.riveInputs.isMoving.value = true)
+		this.riveInputs.clothes.value = 2
 
 		this.mouse = new mouse(this.riveInputs)
 		this.scroll = new fixedScroll(this.riveInputs, this.mouse)
@@ -79,16 +85,16 @@ class App {
 		this.riveResume = new rive.Rive({
 			src: file,
 			artboard: 'Resume',
-			stateMachines: 'controller',
+			stateMachines: 're-controller',
 			canvas: canvases.resume,
-			// layout: new rive.Layout({
-			// 	fit: rive.Fit.FitHeight
-			// }),
 			autoplay: true,
 			onLoad: e => {
-				this.rive.resizeDrawingSurfaceToCanvas()
+				console.log('load resume')
+				// this.rive.resizeDrawingSurfaceToCanvas()
 			},
-			// onStateChange: e => { this.stateRive(e) }
+			onStateChange: e => {
+				console.log(e.data)
+			}
 		})
 
 	}
